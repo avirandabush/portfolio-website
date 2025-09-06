9699a1759001e688b3af978d4ce08be6be827601
Aviran Dabush <avirandabush@gmail.com>
2025-09-06 19:28:01 +0300

    set app flex layout and border.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css"
 import { useState } from "react"
 import { Screen } from "./types/Screen"
 import TopBar from "./components/TopBar"
+import BottomDock from "./components/BottomDock"
 
 function App() {
 
@@ -14,11 +15,15 @@ function App() {
           onNavigate={setActiveScreen}
         />
 
-        {activeScreen === Screen.Home && <h1>Home</h1>}
-        {activeScreen === Screen.About && <h1>About</h1>}
-        {activeScreen === Screen.iOS && <h1>iOS</h1>}
-        {activeScreen === Screen.Android && <h1>Android</h1>}
-        {activeScreen === Screen.React && <h1>React</h1>}
+        <div className="content">
+          {activeScreen === Screen.Home && <h1>Home</h1>}
+          {activeScreen === Screen.About && <h1>About</h1>}
+          {activeScreen === Screen.iOS && <h1>iOS</h1>}
+          {activeScreen === Screen.Android && <h1>Android</h1>}
+          {activeScreen === Screen.React && <h1>React</h1>}
+        </div>
+
+        <BottomDock activeScreen={activeScreen} />
       </div>
     </div>
   )
