c8b6cedb0c84e8676b90252950e4fd41ea7f90ef
Aviran Dabush <avirandabush@gmail.com>
2025-09-15 16:59:38 +0300

    project interface.

---
+++ src/types/Project.ts
@@ -0,0 +1,13 @@
+export type systemType = "ios" | "android" | "desktop"
+
+export interface Project {
+    id: string
+    name: string
+    description: string
+    videoUrl: string
+    codeUrl: string
+    liveUrl: string
+    createdAt: Date
+    tags: string[]
+    type: systemType
+}
