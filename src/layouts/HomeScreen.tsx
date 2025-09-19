81b63b358bf90b21ee6cb11dd96505092d1042e7
Aviran Dabush <avirandabush@gmail.com>
2025-09-19 16:42:50 +0300

    home ui for mobile display.

--- src/layouts/HomeScreen.tsx
+++ src/layouts/HomeScreen.tsx
@@ -1,8 +1,10 @@
+import { useDevice } from "../context/DeviceContext";
 import "./Home.css"
 
 const HomeScreen = () => {
+    const { isMobile } = useDevice();
     return (
-        <div className="home-container">
+        <div className={`home-container ${isMobile ? 'mobile' : 'desktop'}`}>
             <h1>Aviran Dabush</h1>
             <h2>Front-End Developer</h2>
         </div>
