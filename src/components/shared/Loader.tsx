15f8551e190af481a5f89be1edba67b18440cfee
Aviran Dabush <avirandabush@gmail.com>
2025-09-24 11:21:36 +0300

    create loader.

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,19 @@
+import "./Loader.css"
+
+export default function Loader() {
+    return (
+        <>
+            <div className="ai-matrix-loader">
+                <div className="digit">0</div>
+                <div className="digit">1</div>
+                <div className="digit">0</div>
+                <div className="digit">1</div>
+                <div className="digit">1</div>
+                <div className="digit">0</div>
+                <div className="digit">0</div>
+                <div className="digit">1</div>
+                <div className="glow"></div>
+            </div>
+        </>
+    )
+}
